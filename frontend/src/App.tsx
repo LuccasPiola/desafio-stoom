2bf1750d2b926f04ca06168888d4d082e3ddc95d
Luccas Piola <luccas.piola@sciensa.com>
2021-02-02 21:04:23 -0300

    feature: Initial configuration

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import GlobalStyle from './styles/global'
+
+export default function App(): JSX.Element {
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Hello World</h1>
+    </>
+  )
+}
---
+++ frontend/src/styles/global.ts
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  body {
+    background: #312e38;
+    color: #fafafa;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  html, body, #root {
+    width: 100%;
+    height: 100%;
+  }
+
+  body, input, button {
+    font-family: 'Roboto Slab', serif;
+    font-size: 16px;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`
+
+export default GlobalStyle
